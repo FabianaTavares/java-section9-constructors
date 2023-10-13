73417a20bef5139cb1d10b52b070ac5f42a26dd5
Fabiana Tavares <fabi.tavares1@gmail.com>
2023-10-13 15:44:13 -0300

    first commit

---
+++ src/entities/Product.java
@@ -0,0 +1,41 @@
+package entities;
+
+public class Product {
+    public String name;
+    public double price;
+    public int quantity;
+
+    public Product(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    //sobrecarga é oferecer mais de uma versão da mesma operação, a diferença é a lista de parametros.
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public double totalValueInStock() {
+        return price * quantity;
+    }
+
+    public void addProducts(int quantity) {
+        this.quantity += quantity;
+    }
+
+    public void removeProducts(int quantity) {
+        this.quantity -= quantity;
+    }
+
+    public String toString() {
+        return name
+                + ", $ "
+                + String.format("%.2f", price)
+                + ", "
+                + quantity
+                + " units, Total: $ "
+                + String.format("%.2f", totalValueInStock());
+    }
+}
